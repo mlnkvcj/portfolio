379d5973d2b00203c43c828daff1a9d17579f337
mlnkvcj <milinkovicjovan05@gmail.com>
2023-12-25 02:51:16 +0100

    Dodata nova stranica sa projektima(dinamičko renderovanje)

---
+++ projects.js
@@ -0,0 +1,82 @@
+projects = [
+  {
+    "title": "Dostavljač",
+    "tag": ["Web"],
+    "text": "Ovaj projekat uključuje razvoj web aplikacije za dostavu.",
+    "imageURL": "https://example.com/slika1.jpg"
+  },
+  {
+    "title": "Analiza podataka",
+    "tag": ["Python"],
+    "text": "Analiza podataka i vizualizacija korišćenjem Pythona i odgovarajućih biblioteka.",
+    "imageURL": "https://example.com/slika2.jpg"
+  },
+  {
+    "title": "Flappy bird",
+    "tag": ["C++"],
+    "text": "Razvoj jednostavne igre 'Flappy bird' koristeći C++.",
+    "imageURL": "https://example.com/slika3.jpg"
+  },
+  {
+    "title": "Snake",
+    "tag": ["C++"],
+    "text": "Razvoj igre 'Snake' koristeći C++.",
+    "imageURL": "https://example.com/slika4.jpg"
+  },
+  {
+    "title": "Baza podataka za zubnu ordinaciju",
+    "tag": ["SQL"],
+    "text": "Razvoj baze podataka za zubnu ordinaciju koristeći SQL.",
+    "imageURL": "https://example.com/slika5.jpg"
+  },
+  {
+    "title": "Baza podataka za izlozbu pasa",
+    "tag": ["SQL"],
+    "text": "Razvoj baze podataka za izlozbu pasa koristeći SQL.",
+    "imageURL": "https://example.com/slika6.jpg"
+  },
+  {
+    "title": "Baza podataka za auto-radionicu",
+    "tag": ["SQL"],
+    "text": "Razvoj baze podataka za auto-radionicu koristeći SQL.",
+    "imageURL": "https://example.com/slika7.jpg"
+  },
+  {
+    "title": "Baza podataka za motel",
+    "tag": ["SQL"],
+    "text": "Razvoj baze podataka za motel koristeći SQL.",
+    "imageURL": "https://example.com/slika8.jpg"
+  },
+  {
+    "title": "Flappy bird - Python",
+    "tag": ["Python"],
+    "text": "Izrada 'Flappy bird' igrice koristeči Python.",
+    "imageURL": "https://example.com/slika9.jpg"
+  },
+  {
+    "title": "Notes",
+    "tag": ["C#"],
+    "text": "Izrada notes aplikacije koristeći C# programski jezik.",
+    "imageURL": "https://example.com/slika10.jpg"
+  },
+  {
+    "title": "Space invaders",
+    "tag": ["Python"],
+    "text": "Izrada 'Space invaders' igrice koristeči Python.",
+    "imageURL": "https://example.com/slika11.jpg"
+  },
+  {
+    "title": "Portfolio vebsajt",
+    "tag": ["Web"],
+    "text": "Ovaj projekat uključuje razvoj ličnog portfolio vebsajta.",
+    "imageURL": "https://example.com/slika12.jpg"
+  },
+  {
+    "title": "Kalkulator",
+    "tag": ["C#"],
+    "text": "Izrada kalkulatora koristeći C# programski jezik.",
+    "imageURL": "https://example.com/slika13.jpg"
+  }
+]
+
+  
